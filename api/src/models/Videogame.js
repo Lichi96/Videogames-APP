6256e28e7b3475a1c2e205612fa8310a1c830a66
Lichi96 <lichilanda@hotmail.com>
2022-06-19 20:38:03 -0300

    Modelo Videogame

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -2,11 +2,43 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	// defino el modelo
+	sequelize.define('videogame', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false
+		},
+		released: {
+			type: DataTypes.DATEONLY,
+			defaultValue: DataTypes.NOW
+		},
+		rating: {
+			type: DataTypes.FLOAT,
+			defaultValue: 0
+		},
+		platforms: {
+			type: DataTypes.ARRAY(DataTypes.STRING),
+			allowNull: false
+		},
+		image: {
+			type: DataTypes.STRING(1234)
+		},
+		createdInDB: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: true
+		}
+	}, {
+		timestamps: false
+	});
 };
