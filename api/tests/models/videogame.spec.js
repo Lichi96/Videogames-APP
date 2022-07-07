e15e8706ff8f1efeb480d85e9f49241ab3837646
Lichi96 <lichilanda@hotmail.com>
2022-07-07 00:03:09 -0300

    Ultimos cambios en el backend

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -7,7 +7,7 @@ describe('Videogame model', () => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
-    beforeEach(() => Videogame.sync({ force: true }));
+    beforeEach(() => Videogame.sync({ force: false }));
     describe('name', () => {
       it('should throw an error if name is null', (done) => {
         Videogame.create({})
@@ -15,8 +15,19 @@ describe('Videogame model', () => {
           .catch(() => done());
       });
       it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Super Mario Bros' });
+        Videogame.create({ name: 'Super Mario Bros' });
       });
     });
+    describe('description', () => {
+      it('should throw an error if description is null', (done) => {
+        Videogame.create({})
+          .then(() => done(new Error('It requires a valid description')))
+          .catch(() => done());
+      });
+      it('should work whne its a valid description', () => {
+        Videogame.create({ description: 'A random description'})
+      });
+    })
+
   });
 });
